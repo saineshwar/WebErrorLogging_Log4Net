625503ae573b04dd9ffe88d7d27b99915f661768
saineshwar bageri <saihacksoft@gmail.com>
2019-02-23 00:15:46 +0530

    First Commit
    First Commit

---
+++ WebErrorLogging/WebErrorLogging/Controllers/DefaultController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using log4net;
+
+namespace WebErrorLogging.Controllers
+{
+    public class DefaultController : Controller
+    {
+        private static readonly ILog Log = LogManager.GetLogger(typeof(DefaultController));
+        // GET: Default
+        public ActionResult Index()
+        {
+            try
+            {
+                Log.Debug("Hi I am log4net Debug Level");
+                Log.Info("Hi I am log4net Info Level");
+                Log.Warn("Hi I am log4net Warn Level");
+                throw new NullReferenceException();
+                return View();
+            }
+            catch (Exception ex)
+            {
+                Log.Error("Hi I am log4net Error Level", ex);
+                Log.Fatal("Hi I am log4net Fatal Level", ex);
+                throw;
+            }
+        }
+    }
+}
